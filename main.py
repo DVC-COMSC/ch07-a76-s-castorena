3edc1557e9ec6f7ab7fb12622ad9097eafa0c881
sarah castorena <ceanz@yahoo.com>
2022-10-18 08:05:46 -0700

    new

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 
-num1 = list(map(int, input().split()))
-num2 = list(map(int, input().split()))
+num1 = list(map(int, input('Enter first list').split()))
+num2 = list(map(int, input('Enter second list').split()))
 
 # ******************************
 # Make your Code
 # ******************************
+if (len(num1) < len(num2)) or (len(num1)==len(num2)):
+   num3 = num1 + num2
+elif len(num2) < len(num1):
+   num3 = num2 + num1   
 
-# print (num3) 
+
+
+print (num3) 
